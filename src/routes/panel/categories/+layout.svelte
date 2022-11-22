cb0abeef72fc6597a82be20fcc0941ba03bc7b94
unknown <kelvinj542@hotmail.com>
2022-11-22 15:43:39 -0400

    panel improvements

---
+++ src/routes/panel/categories/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data;
+    $: console.log(data);
+</script>
+
+<div class="tabs-box">
+    <div class="tabs ">
+        
+        <a href="/panel/categories" class="tab tab-bordered" class:tab-active={data?.path === 'index'}>General</a> 
+        <a href="/panel/categories/add" class="tab tab-bordered" class:tab-active={data?.path === 'add'}>Nuevo</a> 
+        <a href="/panel/categories/actions" class="tab tab-bordered">Acciones</a>
+    </div>
+</div>
+
+<slot/>
+
+<style>
+    .tabs-box {
+        width:100%;
+        display:flex;
+        justify-content: center;
+    }
+</style>
---
+++ src/routes/panel/products/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data;
+    $: console.log(data);
+</script>
+
+<div class="tabs-box">
+    <div class="tabs ">
+        
+        <a href="/panel/products" class="tab tab-bordered" class:tab-active={data?.path === 'index'}>General</a> 
+        <a href="/panel/products/add" class="tab tab-bordered" class:tab-active={data?.path === 'add'}>Nuevo</a> 
+        <a href="/panel/products/actions" class="tab tab-bordered">Acciones</a>
+    </div>
+</div>
+
+<slot/>
+
+<style>
+    .tabs-box {
+        width:100%;
+        display:flex;
+        justify-content: center;
+    }
+</style>
