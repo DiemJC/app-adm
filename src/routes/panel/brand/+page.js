dcc5beb862bf5e469ec63c07f37941cb018ec954
angeldesweb <angeldesweb@mail.com>
2022-11-22 20:33:24 -0400

    doc:some notes

--- src/routes/panel/brand/+page.js
+++ src/routes/panel/brand/+page.js
@@ -9,15 +9,14 @@ export const load = async ({fetch}) => {
     
     if(!session) browser && goto('/');
 
-    const response = await fetch('http://localhost:3001/pub/brand/list');
-    const data = await response.json();
-    const { success , docs } = data;
+    const response = await fetch('http://localhost:3001/pub/brand/list'); //brand - category - product
+    const data = await response.json(); // Esperar respuesta del back y convertir en json
+    const { success , docs } = data; //Respuesta del back
 
-    if(!success) return {
-        error:true
-    }
+    if(!success) return {error:true}
 
-    let size = docs ? docs.length : 0;
+    let size = docs ? docs.length : 0; //Devuelve la cantidad de documentos obtenidos
+
     return {
         success,
         docs,
