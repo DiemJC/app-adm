30dca6b400558163dc67ebc33fb7539a21fe8ce9
angeldesweb <angeldesweb@mail.com>
2022-11-23 14:45:49 -0400

    feature: subs phase demo

--- src/routes/panel/categories/subcategory/[id]/update/+page.svelte
+++ src/routes/panel/categories/subcategory/[id]/update/+page.svelte
@@ -1,5 +1,47 @@
 <script>
+    import { goto } from '$app/navigation';
+    import { env } from '$env/dynamic/public';
+
+    import { showToastBR } from '$lib/tools/toasts';
     export let data;
+
+    const updateSub = async (id) => {
+        const label = document.getElementById(id).value;
+        try {
+            const req = await fetch(`${env.PUBLIC_API_URL}/api/sub/update/${id}`,
+            {
+                method:'PUT',
+                headers:{
+                    'Content-type':'application/json'
+                },
+                body:JSON.stringify({label})
+            })
+            const { success , message } = await req.json();
+            showToastBR({title:'Actualizado',message,type:'success'});
+            goto('/panel/categories/subcategory')
+        } catch (error) {
+            showToastBR({title:'Error',message:error,type:'error'});
+        }
+    } 
+
+    const deleteSub = async (id) => {
+        const proceed = await confirm('¿Seguro desea eliminar?')
+        if(!proceed) return;
+        try {
+            const req = await fetch(`${env.PUBLIC_API_URL}/api/sub/delete/${id}`,
+            {
+                method:'DELETE',
+                headers:{
+                    'Content-type':'application/json'
+                }
+            })
+            const { message } = await req.json();
+            showToastBR({title:'Eliminado',message,type:'success'});
+            goto('/panel/categories/subcategory')
+        } catch (error) {
+            showToastBR({title:'Error',message:error,type:'error'});
+        }
+    } 
 </script>
 
 <a href="/panel/category/subcategory" class="link link-hover text-primary ml-2">Regresar</a>
@@ -11,10 +53,10 @@
     {#each data.docs as doc}
     <div class="card shadow-xl p-6 m-auto" style="width:min(30rem,100%);margin-top:2rem;" >
         <span class="text text-primary">Subcategoría</span>
-        <input type="text" class="input input-bordered" value="{doc.label}">
+        <input id={doc._id} type="text" class="input input-bordered" value="{doc.label}">
         <div class="flex gap-2">
-            <button class="btn btn-ghost text-success">Actualizar</button>
-            <button class="btn btn-ghost text-error">Eliminar</button>
+            <button class="btn btn-ghost text-success" on:click={() => updateSub(doc._id)}>Actualizar</button>
+            <button class="btn btn-ghost text-error" on:click={() => deleteSub(doc._id)}>Eliminar</button>
         </div>
     </div>
     {/each}
