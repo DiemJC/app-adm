ff94f9d77a57b34691b86bc919b4d020f2a4cca6
angeldesweb <angeldesweb@mail.com>
2022-11-22 22:37:46 -0400

    fix: some fixes

--- src/routes/panel/categories/+page.js
+++ src/routes/panel/categories/+page.js
@@ -9,13 +9,13 @@ export const load = async ({fetch}) => {
     
     if(!session) browser && goto('/');
 
-    const response = await fetch('http://localhost:3001/pub/category/list'); //brand - category - product
-    const data = await response.json(); // Esperar respuesta del back y convertir en json
-    const { success , docs } = data; //Respuesta del back
+    const response = await fetch('http://localhost:3001/pub/category/list');
+    const data = await response.json(); 
+    const { success , docs } = data; 
 
     if(!success) return {error:true}
 
-    let size = docs ? docs.length : 0; //Devuelve la cantidad de documentos obtenidos
+    let size = docs ? docs.length : 0; 
 
     return {
         success,
