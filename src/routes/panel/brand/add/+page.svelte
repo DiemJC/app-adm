fb4b1cfd07a3771c8a120916208a61df775978a9
unknown <kelvinj542@hotmail.com>
2022-11-24 19:46:19 -0400

    forms refactor for brand and categories

--- src/routes/panel/brand/add/+page.svelte
+++ src/routes/panel/brand/add/+page.svelte
@@ -1,39 +1,34 @@
 <script>
-    import { enhance } from '$app/forms';
+    import { FormBox, Field } from '$lib/components';
     import { goto } from '$app/navigation';
     import { showToastBR } from '$lib/tools/toasts';
     
     export let form;
+
+    $: NameError = '';
 
     $: if(form?.success){ 
         showToastBR({title:form.message,message:'Su registro fue realizado con éxito',type:'success'});
         goto('/panel/brand')
     }
 
+    $: if(form?.name && form.missing) NameError = 'Debe ingresar un nombre';
+    
 </script>
 <svelte:head>
     <title>Nueva Marca</title>
 </svelte:head>
 
-<div class="view text-center">
-    <div class="box">
-        <h2 class="text-3xl text-primary">Registrar nueva marca</h2>
-        <div class="divider"></div>
-        <form method="POST" use:enhance>
-            <div class="form-group">
-                <label for="" class="label text-sm">Nombre</label>
-                <input name="name" type="text" class:input-error={form?.name && form.missing} class="input input-bordered form-control w-full" placeholder="ejemplo@mail.com" >
-            </div>
-            <div class="form-group">
-                <label for="" class="label text-sm">Descripción</label>
-                <textarea name="slug" class:textarea-error={form?.slug && form.missing} class="textarea textarea-bordered w-full" placeholder="Breve descripción del producto" />
-            </div>
-            <br>
-            <div class="flex flex-end">
-                <button class="btn btn-primary text-white">Registrar</button>
-            </div>
-        </form>
-    </div>
+<div class="view text-center box">
+    <FormBox btn="Registrar" title="Registrar nueva marca">
+        <Field 
+            name="name" type="text" ph="Marca" label="Nombre" 
+            warning={form?.name && form.missing || form?.short} fb={NameError} 
+        />
+        <Field 
+            name="slug" type="textarea" ph="Descripción breve" label="Descripción" 
+        />
+    </FormBox>
 </div>
 
 <style>
