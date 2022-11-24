2540d9804baf918606fcfc6153e33385b8e40ca9
angeldesweb <angeldesweb@mail.com>
2022-11-24 13:12:08 -0400

    refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,20 +18,21 @@
     $: if(form?.email && form.missing) emailError = 'Debe ingresar un correo electrónico';
     $: if(form?.password && form.missing) passwordError = 'Debe ingresar una contraseña';
     $: if(form?.password && form?.short) passwordError = 'La contraseña es demasiado corta';
-    
+
 </script>
 
 <div class="form-box">
     <div class="form" style="width: min(40rem,100%);">
         <h2 class="text-3xl">Inicio de sesión</h2>
         <form method="POST" use:enhance>
-            <Field name="email" type="email" ph="ejemplo@mail.com" 
-                label="Correo electrónico" warning={form?.email && form.missing} 
-                fb={emailError}
+            <Field 
+                name="email" type="email" ph="ejemplo@mail.com" label="Correo electrónico" 
+                warning={form?.email && form.missing} fb={emailError}
             />
-            <Field name="password" type="password" ph="Ingrese su contraseña" 
-                label="Contraseña"
-                warning={form?.password && form.missing || form?.short} fb={passwordError} />
+            <Field 
+                name="password" type="password" ph="Ingrese su contraseña" label="Contraseña"
+                warning={form?.password && form.missing || form?.short} fb={passwordError} 
+            />
             <br>
             <button class="btn btn-primary text-white">Iniciar sesión</button>
         </form>
